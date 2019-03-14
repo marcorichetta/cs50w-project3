230d9ed756231b5c901b74424eb400465b158e13
Marco Richetta <marcorichetta@gmail.com>
2019-03-14 16:03:56 -0300

    Created main and extra models

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Pizza, Topping
+from .models import Pizza, Topping, Pasta, Salad, DinnerPlatter, Sub, SubExtra
 
 # Register your models here.
 
 admin.site.register(Pizza)
 admin.site.register(Topping)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(DinnerPlatter)
+admin.site.register(Sub)
+admin.site.register(SubExtra)
--- orders/models.py
+++ orders/models.py
@@ -2,18 +2,58 @@ from django.db import models
 
 # Create your models here.
 
+class Pasta(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.DecimalField(help_text="Price in U$S",max_digits=4, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.name} - $ {self.price}"
+
+class Salad(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.DecimalField(help_text="Price in U$S",max_digits=4, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.name} - $ {self.price}"
+
+class DinnerPlatter(models.Model):
+    name = models.CharField(max_length=40)
+    size = models.CharField(max_length=10, choices=(('S', 'Small'), ('L', 'Large')))
+    price = models.DecimalField(help_text="Price in U$S", max_digits=5, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.name} - {self.get_size_display()} - $ {self.price}"
+
+class SubExtra(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(help_text="Price in U$S", max_digits=4, decimal_places=2) 
+
+    def __str__(self):
+        return f"{self.name} - $ {self.price}"
+
+class Sub(models.Model):
+    name = models.CharField(max_length=40)
+    size = models.CharField(max_length=10, choices=(('S', 'Small'), ('L', 'Large')))
+    price = models.DecimalField(help_text="Price in U$S", max_digits=4, decimal_places=2)
+    extras = models.ManyToManyField(SubExtra, blank=True)
+
+    def __str__(self):
+        if self.extras.count() == 0:
+            return f"{self.name} - {self.get_size_display()} - $ {self.price} - Extras: 0"
+        else:
+            return f"{self.name} - {self.get_size_display()} - $ {self.price} - Extras: {self.extras.in_bulk()}"
+
 class Topping(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
 
     def __str__(self):
         return f"{self.name}"
 
 class Pizza(models.Model):
-    tipo = models.CharField(max_length=10, choices=(('R', 'Regular'), ('S', 'Sicilian')))
+    style = models.CharField(max_length=10, choices=(('R', 'Regular'), ('S', 'Sicilian')))
     size = models.CharField(max_length=10, choices=(('S', 'Small'), ('L', 'Large')))
-    precio = models.IntegerField()
+    price = models.IntegerField(help_text="Price in U$S")
     toppings = models.ManyToManyField(Topping)
-    #extras = models.ForeignKey(Topping, on_delete=models.CASCADE, related_name="condimentos")
 
     def __str__(self):
-        return f"{self.tipo} - {self.size} - {self.precio} - Toppings: {self.toppings.in_bulk().values()}"
+        return f"{self.get_style_display()} - {self.get_size_display()} - {self.price} - Toppings: {self.toppings.in_bulk()}"
