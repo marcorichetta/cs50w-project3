68c30c6ac3730f2f647fe3c9476d7d5a3eadfe00
Marco Richetta <marcorichetta@gmail.com>
2019-03-15 20:35:26 -0300

    Implemented user registration
    - Created new app 'users'
    - Created layout.html
    - Added Bootstrap files (Previous commit)
    - Added Crispy Forms apps

--- pizza/urls.py
+++ pizza/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from users import views as user_views
 
 urlpatterns = [
     path("", include("orders.urls")),
+    path("register/", user_views.register, name='register'),
     path("admin/", admin.site.urls),
 ]
