4423fd3071ceadd3cd4652107cca47914354d652
Marco Richetta <marcorichetta@gmail.com>
2019-03-23 17:38:10 -0300

    User login/out implementation completed
    - Created users app to deal with all the logic there.
    - Now users can login, logout and signup.
    - Crispy forms with Bootstrap 4 chosen.
    - Layout.html navbar changes depending on authentication.

--- users/views.py
+++ users/views.py
@@ -11,8 +11,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'{username}, your user has been created!')
-            return redirect('index')
+            messages.success(request, f'{username}, your user has been created. Please, log in!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
