abb404778874f562b3e9416fba345791acb7ea1d
Marco Richetta <marcorichetta@gmail.com>
2019-04-03 12:15:03 -0300

    Created PizzaForm Model
    - Added orders/forms.py
    - Views.py instanciates the PizzaForm and passes it to index.html
    
    TODO:
    - Validate the form
    - Style the UI

--- orders/views.py
+++ orders/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from orders.models import Pizza, Pasta, Salad, DinnerPlatter, SubExtra, Sub, Topping
+from orders.forms import PizzaForm
 
 # Create your views here.
 
 @login_required(login_url='/login/')
 def index(request):
 
+    form = PizzaForm()
+
     context = {
-        "pizzas": Pizza.objects.all(),
         "toppings": Topping.objects.all(),
         "subs": Sub.objects.all(),
-        "subExtras": SubExtra.objects.all()
+        "subExtras": SubExtra.objects.all(),
+        "form": form
     }
 
     return render(request, "orders/index.html", context)
