aaab797790e09ddb1005fa7d43e05ac9d21e42eb
Marco Richetta <marcorichetta@gmail.com>
2019-04-03 11:37:29 -0300

    Added comments

--- users/views.py
+++ users/views.py
@@ -7,12 +7,16 @@ from users.forms import UserRegisterForm
 def register(request):
     """ Register a new user """
     if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding)
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            # Retrieve username from cleaned_data to show it on message
             username = form.cleaned_data.get('username')
             messages.success(request, f'{username}, your user has been created. Please, log in!')
             return redirect('login')
+    # GET Request
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
